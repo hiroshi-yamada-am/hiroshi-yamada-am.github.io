7728d83c36b38cc1e4f42b80b4df681827d7e3e1
hiroshi-yamada-am <127813789+hiroshi-yamada-am@users.noreply.github.com>
2023-04-26 10:04:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ function clickBackground(){
     const buttonBackgroundElement = document.getElementById('buttonBackground');
     const newElement = document.createElement('ol');
     const li1Element = document.createElement('li');
-    li1Element.innerHTML = '自分の企画への共感者・仲間づくり';
+    li1Element.innerHTML = '実証実験用のアプリ内製化に向けたプログラミング力アップ';
     const li2Element = document.createElement('li');
-    li2Element.innerHTML = '実証実験用のアプリ内製化';
+    li2Element.innerHTML = '自分の企画への共感者・仲間づくり';
     backGroundElement.appendChild(newElement);
     newElement.appendChild(li1Element);
     newElement.appendChild(li2Element);
     buttonBackgroundElement.disabled = "disabled";
+}
+
+function clickResult(){
+    const resultElement = document.getElementById('result');
+    const buttonResultElement = document.getElementById('buttonResult');
+    const newElement = document.createElement('h3');
+    newElement.innerHTML = '(1)本業の実証用アプリ開発が全然スケジュール通りに進まない、<br>(2)自宅の引っ越し、(3)子供の転園という三重苦の日々でしたが、<br>楽しいペアプロの時間を通し、人に説明できるまで習得する大切さを学びました！<br><br>特にHTMLやCSS、DOMは全く知らない世界で一番ワクワクしたため、<br>今回はそれらを活用した自分の企画の紹介ページを作ってみました。<br><br>プログラミング力を活かして社会貢献する一つの事例として参考に聞いてもらえればと思います。';
+    resultElement.appendChild(newElement);
+    buttonResultElement.disabled = "disabled";
 }
 
 function clickMyJob(){
@@ -28,7 +37,7 @@ function clickMyDream(){
     const myDreamElement = document.getElementById('myDream');
     const buttonMyDreamElement = document.getElementById('buttonMyDream');
     const newElement = document.createElement('h3');
-    newElement.innerHTML = 'クルマを卒業された方の移動の課題を解決したい！';
+    newElement.innerHTML = 'クルマを卒業（免許返納）された方の移動の課題を解決したい！';
     myDreamElement.appendChild(newElement);
     buttonMyDreamElement.disabled = "disabled";
 }
@@ -36,14 +45,18 @@ function clickMyDream(){
 function clickTitle(){
     const titleElement = document.getElementById('title');
     const buttonTitleElement = document.getElementById('buttonTitle');
-    const newImgTitleElement1 = document.createElement('img');
+    // const newImgTitleElement1 = document.createElement('img');
     const newImgTitleElement2 = document.createElement('img');
-    newImgTitleElement1.src = "title.png";
-    newImgTitleElement2.src = "symptoms.png";
-    titleElement.appendChild(document.createElement("br"));
-    titleElement.appendChild(newImgTitleElement1);
+    const newImgTitleElement3 = document.createElement('img');
+    // newImgTitleElement1.src = "title.png";
+    newImgTitleElement2.src = "productOutline.png";
+    newImgTitleElement3.src = "symptoms.png";
+    // titleElement.appendChild(document.createElement("br"));
+    // titleElement.appendChild(newImgTitleElement1);
     titleElement.appendChild(document.createElement("br"));
     titleElement.appendChild(newImgTitleElement2);
+    titleElement.appendChild(document.createElement("br"));
+    titleElement.appendChild(newImgTitleElement3);
     buttonTitleElement.disabled = "disabled";
 }
 
@@ -95,7 +108,7 @@ function clickFinal(){
     const buttonFinalElement = document.getElementById('buttonFinal');
     const newElement = document.createElement('h3');
     const newImgFinalElement = document.createElement('img');
-    newElement.innerHTML = '実現を待ってくれている多くの方のために、内製開発（現状は委託開発メイン）でスピーディーに改善を進めたいものの、<br>まだまだスキルと時間が足りず困っています。<br>もし共感した、興味あるなどあれば、いつでもお気軽にご連絡ください！';
+    newElement.innerHTML = '実現を待ってくれている多くの方のために、内製（現状委託メイン）でスピーディーに改善を進めたいものの、<br>熱意はあるが、スキルと時間がまだまだ足りない状況です。<br>もし共感した、興味あるなどあれば、いつでも本当にお気軽にご連絡ください！<br>20日間本当にありがとうございました。';
     newImgFinalElement.src = "dream.png";
     finalElement.appendChild(newElement);
     finalElement.appendChild(newImgFinalElement);
